7c708d22731e443512b8e694872389769c6e564f
Thomas Dominik <dominik.thomasdl@gmail.com>
2022-07-06 17:45:02 +0200

    Register CSV generate structure command

---
+++ src/Command/CSVGenerateStructureCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tomdkd\ExcelDatabaseImporter\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class CSVGenerateStructureCommand extends Command
+{
+
+    protected static $defaultName = 'database:csv:generate-structure';
+
+    protected function configure()
+    {
+        $this->setDescription('Try your CSV file and generate an example of your future database structure.');
+        $this->addOption('validate', 'val', InputArgument::OPTIONAL, 'Only validate database structure.');
+
+        $this->addArgument('file_path', InputArgument::REQUIRED, 'Path to the CSV file to analyze.');
+        $this->addArgument('output_folder', InputArgument::OPTIONAL, 'Path to output folder.');
+
+        parent::configure();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        return Command::SUCCESS;
+    }
+
+}
